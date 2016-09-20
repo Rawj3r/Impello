e58fd5cd4b518846c32f36109f8c489049e9ccac
Rawj3r <rogermphile1991@gmail.com>
2016-09-20 12:07:23 +0200

    Cart adapter

--- app/src/main/java/za/co/roger/nkosi/impello/ListCart.java
+++ app/src/main/java/za/co/roger/nkosi/impello/ListCart.java
@@ -3,6 +3,10 @@ package za.co.roger.nkosi.impello;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -10,6 +14,7 @@ public class ListCart extends AppCompatActivity implements APIController.GetCart
 
     private APIController controller;
     private RecyclerView recyclerView;
+    private CartAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +47,46 @@ public class ListCart extends AppCompatActivity implements APIController.GetCart
     @Override
     public void onFetchFailed() {
 
+    }
+
+    public class CartAdapter extends RecyclerView.Adapter<CartAdapter.Holder>{
+        private List<CartModel> cartModels;
+
+        public CartAdapter(List<CartModel> cartModels) {
+            this.cartModels = cartModels;
+        }
+
+        public void populate(CartModel model){
+            cartModels.add(model);
+            notifyDataSetChanged();
+
+        }
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_row, parent, false);
+            return new Holder(row);
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            final CartModel model = cartModels.get(position);
+            holder.quantity.setText(model.quantity);
+            holder.pname.setText(model.pname);
+        }
+
+        @Override
+        public int getItemCount() {
+            return cartModels.size();
+        }
+
+        public class Holder extends RecyclerView.ViewHolder {
+            public TextView pname, quantity;
+            public Holder(View itemView) {
+                super(itemView);
+                pname = (TextView)itemView.findViewById(R.id.p_name);
+                quantity = (TextView)itemView.findViewById(R.id.quantity);
+            }
+        }
     }
 }
